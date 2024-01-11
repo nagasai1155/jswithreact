1766999e43e0955053e8a623d92c9da3946e8ea8
nagasai1155 <122740753+nagasai1155@users.noreply.github.com>
2024-01-11 11:53:28 +0530

    Add files via upload

---
+++ naga.js
@@ -0,0 +1,67 @@
+// console.log("hello world");
+// var x = true;
+// console.log(typeof x);
+// console.log(name);
+// var name = "naga";
+// var name= "sai";
+// console.log(name);
+
+// //block level scope
+// {
+//     let name2="hello";
+//     console.log(name2);
+// }
+// console.log(name2);
+// let x = 4;
+// console.log(x++);
+// console.log(x);
+
+// // let y = 5;
+// // console.log(++y);
+// // console.log(y);
+
+// // let a = 4%2;
+// // console.log(a);
+
+// let b = true;
+// let a = true;
+// console.log(!b);
+
+// let x =4;
+// let y=2;
+// let apple= x+=y
+// console.log(apple);
+
+
+// //ternary operator
+// const ds = 4;
+// const sd=3;
+// const ifd = ds<sd?"hello":23;
+// console.log(ifd);
+
+// let xy= "4";
+// let yx = "5";
+// console.log(xy*yx);
+// let ar =isNaN("ads");
+// if(ar){
+//     console.log("hi");
+// }else{
+//     console.log("we");
+// }
+
+
+var appe;
+if(appe=isNaN(242)){
+    console.log("23")
+}
+else{
+    console.log("25")
+}
+
+// var x = String(appe);
+// console.log(typeof x);
+// console.log(appe+" "+typeof (x));
+
+
+let naga = parseInt("34adafdfdsfa");
+console.log(naga);
